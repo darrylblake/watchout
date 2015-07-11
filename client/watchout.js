82d7ed793415e02871917295d972778c741936f6
Hack Reactor Students <accounts@hackreactor.com>
2015-07-10 19:58:02 -0700

    Implementing collisions

--- client/watchout.js
+++ client/watchout.js
@@ -1,15 +1,19 @@
 // start slingin' some d3 here.
 // <svg> ..<circle cx="220" cy="30" r="20" style="stroke: #000000; stroke-width: 3; fill: #6666ff;"/>
+var collisions = 0;
 
 var gameOptions = {
   height: 500,
   width: 800,
-  numEnemies: 20
+  numEnemies: 20,
+  enemSize: 20
 }
 
-var Enemy = function(x, y) {
+var Enemy = function(x, y, size) {
+  this.dimensions = [x,y,size];
   this.x = x;
   this.y = y;
+  this.size = size;
 
 }
 
@@ -23,18 +27,35 @@ svg.selectAll('.enemy').data(generateEnemies(gameOptions.numEnemies))
     .append('svg:image')
     .attr('xlink:href', 'asteroid.png')
     .attr('class','enemy')
-    .attr({
-      x: function(d){ return d.x},
-      y: function(d){ return d.y},
-      height: 20,
-      width: 20
-    });
+    .attr(
+    /*  function(d) {
+        var obj = {};
+        obj['x'] = d.x;
+        obj['y'] = d.y;
+        obj['height'] = 20;
+        obj['width'] = 20;
+        return obj;
+      }
+      */
+      // See what's wrong with multiple calling the data function
+      // To set height and width attributes
+        {
+          x: function(d){ return d.x},
+          y: function(d){ return d.y},
+          height: gameOptions.enemSize,
+          width: gameOptions.enemSize
+        }
 
-var user = svg.data([{cx:0, cy:0}]).append('circle')
+      );
+
+
+    
+
+var user = svg.data([{cx:400, cy:400, r: 20}]).append('circle')
   .attr({
-    cx: 500,
-    cy: 250,
-    r: 20
+    cx: function(d){return d.cx},
+    cy: function(d){return d.cy},
+    r: function(d){return d.r}
   })
 
 var drag = d3.behavior.drag()
@@ -42,9 +63,10 @@ var drag = d3.behavior.drag()
     d.cx += d3.event.dx;
     d.cy += d3.event.dy;
         
-    d3.select(this).attr('transform', function(d){ // ??
-      return "translate(" + [d.cx, d.cy] +")"
-    })
+    // d3.select(this).attr('transform', function(d){ // ??
+    //   return "translate(" + [d.cx, d.cy] +")"
+    // })
+    d3.select(this).attr('cx',d.cx).attr('cy',d.cy)
   })
   .on('dragstart', function(){
     d3.select(this).attr('fill','deepPink');
@@ -55,28 +77,45 @@ var drag = d3.behavior.drag()
 
 user.call(drag);
 
-user.on("click", function() {
-  console.log('clicked');
-});
-
 
 function generateEnemies(numEnemies) {
   var results = [];
   for (var i = 0; i < numEnemies; i++) {
-    var x = Math.random() * gameOptions.width;
-    var y = Math.random() * gameOptions.height;
+    var x = Math.round(Math.random() * gameOptions.width);
+    var y = Math.round(Math.random() * gameOptions.height);
     results.push(new Enemy(x, y));
   };
   return results;
 }
 
+function detectCollision(enemy, user) {
+  var userData = user.data()[0];
+  var x1 = enemy.x;
+  var x2 = userData.cx;
+  
+  var y1 = enemy.y
+  var y2 = userData.cy;
+
+  var d = new Date();
+  var distance = Math.sqrt(Math.pow(y2 - y1, 2) + Math.pow(x2 - x1, 2)) - (gameOptions.enemSize/2) - userData.r;
+  if (distance < 100)
+    console.log(d.getTime(), "distance: "+Math.floor(distance)+", user x: "+x2+", user y: "+y2);
+    console.log("enemy x: "+x1+", enemy y: "+y1+"\n\n");
+  if(distance <= 0){
+    console.log("COLLISION!!");
+    collisions++;
+    d3.select('.collisions').text(collisions);
+  }
+}
 
 setInterval(function() {
   svg.selectAll('.enemy').data(generateEnemies(gameOptions.numEnemies))
     .transition()
     .tween("attr", function(d) {
       return function() {
-        detectCollision(d);
+        detectCollision(d, user);
+        //the outer function 'd' is each asteroid
+        //you want to compare the distance between each asteroid 
       }
     })
     .duration(2000)
@@ -87,11 +126,6 @@ setInterval(function() {
       width: 20
     });
 }, 3000);
-
-setInterval(function() {
-
-}, 100);
-
 
 
 /*
